691b261ca5cd6416335308f8c414b9c2b08be0ee
zeavan guo <guo666@stu2021.jnu.edu.cn>
2024-04-20 11:11:34 -1000

    Add AuditParams.java to gdbiddate-ldcia-server-v2

---
+++ bigdata/gdbigdata-accessmiddleserver/src/main/java/edu/jnu/AccessMiddleServer.java
@@ -0,0 +1,17 @@
+package edu.jnu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.ApplicationContext;
+import springfox.documentation.oas.annotations.EnableOpenApi;
+
+@EnableOpenApi
+@EnableCaching
+@SpringBootApplication
+public class AccessMiddleServer {
+    public static void main(String[] args){
+        ApplicationContext app = SpringApplication.run(AccessMiddleServer.class, args);
+    }
+
+}
